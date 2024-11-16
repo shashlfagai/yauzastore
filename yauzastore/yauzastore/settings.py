d7d5ae077a29d582737e5a7887d0d8c6549500d4
Loginov <itsmeegorloginov@gmail.com>
2024-11-16 16:19:07 +0300

    making backup.sql and changing .gitignote

--- yauzastore/yauzastore/settings.py
+++ yauzastore/yauzastore/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['89.223.71.131']
+ALLOWED_HOSTS = ['89.223.71.131', '127.0.0.1']
 
 
 # Application definitionc
